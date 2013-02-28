1ccd03dd711de186e0122085549e8d8bbe47bf3b
Marti Planellas <beldar.cat@gmail.com>
2013-02-28 10:39:30 +0100

    Add no-cache headers so the talk is never cached.

--- talk.php
+++ talk.php
@@ -1,4 +1,6 @@
 <?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 include "programe/respond.php";
 $r = array();
 if(isset($_POST['input']) && !empty($_POST['input']) && isset($_POST['user']) && !empty($_POST['user'])){
